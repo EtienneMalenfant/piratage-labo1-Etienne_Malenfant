6e3a029902d91b38d2f8f6274d0cef736f68f313
EtienneMalenfant <125925730+EtienneMalenfant@users.noreply.github.com>
2024-08-30 14:52:52 -0400

    Ajout de validations

--- passoire.cpp
+++ passoire.cpp
@@ -17,22 +17,42 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	int pansDeCloture { stoi(argv[1])};
-	float montantAPayer { stof(argv[2])};
-	float coutParPan {montantAPayer / pansDeCloture};
+	//Si un des 2 premiers arguments ne peuvent être converti en leur valeurs respective (int et float), le programme ne peut compiler.
+	//L'inconvénient est qu'on ne peut utiliser l'application si on ne spécifie pas les bon arguments. Il vaut mieux quitter l'exécution
+	//et expliquer pourquoi au lieu de planter.
+	//On peut régler le problème en englobant la conversion dans un try catch.
+	int pansDeCloture{ 0 };
+	float montantAPayer{ 0.f };
+	try {
+		pansDeCloture = stoi(argv[1]);
+		montantAPayer = stof(argv[2]);
+	}
+	catch (const exception& e) {
+		cout << "Erreur:, un des argument n'a pu etre converti en valeur numerique. Le premier doit être de type int et le deuxième de type float." << endl << e.what();
+		return -2;
+	}
+	float coutParPan{ montantAPayer / pansDeCloture };
 
 	// Poteaux nécessaires pour faire une clôture en ligne droite
-	int poteaux {pansDeCloture};
+	int poteaux{ pansDeCloture };
 	montantAPayer += poteaux * 5;
 
 	//Si l'argument du nom de fichier dépasse les 10 caractères, ça lève une exeption qui fait planter l'application
-	//On pourrait exploiter la faille pour faire un buffer overflow pour insérer du code malicieux dans la mémoire.
+	//On pourrait exploiter la faille pour faire un buffer overflow pour insérer du code malicieux dans la mémoire. Aussi, si
+	// on dépasse le tableau cela pourrait corrompre les données dans les cases mémoires d'après.
 	//Une solution serait de vérifier la longueur de l'argument avant de le copier, ou d'utliser strncpy() pour pas copier
-	//si la string reçue est trop grosse
+	//si la string reçue est trop grosse.  Je fait de-même pour strcat() en strncat();
 	char nomFichier[10];
 	strncpy(nomFichier, argv[3], sizeof(nomFichier));
-	strcat(nomFichier, ".exe");
-	ofstream fichier {nomFichier};
+	strncat(nomFichier, ".exe", sizeof(nomFichier));
+	ofstream fichier{ nomFichier };
+	//Si le fichier n'a pu être créer, l'application plante
+	//On pourrait faire exprès que le fichier ne peut être créer pour faire planter intentionnelement l'application.
+	//La solution est de vérifier si le fichier est vlaide avant de procédé
+	if (!fichier) {
+		cout << "Erreur, le fichier n'a pu être créer";
+		return -3;
+	}
 	fichier << argv[4];
 
 	//On peut imprimer des caractère spéciaux commençant par %.
